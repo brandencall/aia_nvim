f99bf0b0a2bffcd28671c12f61d6d49a8fef34b8
Branden Call <brandencall@live.com>
2025-06-15 14:56:33 -0600

    starting to work on the plug

--- lua/aia/init.lua
+++ lua/aia/init.lua
@@ -1,4 +1,11 @@
 local M = {}
 
+M.autoComplete = function()
+    local cursorPos = vim.api.nvim_win_get_cursor(0)
+    local row, col = cursorPos[1] - 1, cursorPos[2]
+    local lines = vim.api.nvim_buf_get_lines(0, 0, row - 1, false)
+    local current_line = lines[#lines]:sub(1, col)
+end
+
 
 return M
