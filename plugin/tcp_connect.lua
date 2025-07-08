5060f1eece06a8a8b23814a31918342979774650
Branden Call <brandencall@live.com>
2025-07-08 08:04:45 -0600

    Updated floating window name

--- plugin/tcp_connect.lua
+++ plugin/tcp_connect.lua
@@ -1,4 +1,4 @@
 local ui = require("aia.ui.main");
 
 require("aia").setup_aia()
-vim.api.nvim_create_user_command('FloatingWin', ui.create_floating_win, {})
+vim.api.nvim_create_user_command('AiFloatingWin', ui.create_floating_win, {})
